ebf3b3228f090d81ea95b86fd075aeb1b6c4711e
Seven90 <stevesurya@gmail.com>
2014-06-25 14:49:38 -0400

    Created restaurant controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'restaurants/new'
+
+  get 'restaurants/create'
+
+  get 'restaurants/update'
+
+  get 'restaurants/destroy'
+
+  get 'restaurants/edit'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
